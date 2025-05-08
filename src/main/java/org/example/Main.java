e9b5f9c07bf6a16350ff62611b44b62b629958a4
student-diyajain <diyajain341030@gmail.com>
2025-05-08 13:28:43 +0530

    end

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hii");
+        System.out.println("end");
     }
 }
